d530369bafd91137e4664a8feea500611c5e7e6d
pltnkvart <artosi@yandex.ru>
2023-11-25 22:32:42 +0300

    Some styles for form

--- src/components/ContentBlock/Form/FormDepartment.tsx
+++ src/components/ContentBlock/Form/FormDepartment.tsx
@@ -6,8 +6,14 @@ const FormDepartment: React.FC = observer(() => {
   return (
     <>
       <form action='post' className={styles.formDepartment}>
-        <input type='text' name='newDepartment' id='newDepartment' />
-        <button>Создать</button>
+        <input
+          type='text'
+          name='newDepartment'
+          id='newDepartment'
+          className={styles.inputName}
+          placeholder='Название нового департамента'
+        />
+        <button className={styles.btn}>Создать</button>
       </form>
     </>
   );
