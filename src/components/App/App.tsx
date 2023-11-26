c517ee2c76e31430afa3c1c57a11adb744ca5355
EvgeniiyaR <tulpansik@mail.ru>
2023-11-26 22:01:46 +0300

    feat: add routes login, registration

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,20 +6,46 @@ import Alert from '../Alert/Alert';
 import { observer } from 'mobx-react-lite';
 import alertStore from '@/stores/AlertStore';
 import styles from './app.module.css';
+import Auth from '../Auth/Auth';
 
 const App: React.FC = observer(() => {
   const { isOpen, message, toggleAlert } = alertStore;
+
   return (
     <>
-      <div className={[styles.app, isOpen && styles.openAlert].join(' ')}>
-        <SideBar />
-        <Routes>
-          <Route path='/' element={<ContentBlock />} />
-          <Route path='/categories/:categoryName' element={<ContentBlock />} />
-          <Route path='/trash' element={<ContentBlock />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path='/login' element={<Auth />} />
+        <Route path='/registration' element={<Auth />} />
+        <Route
+          path='/'
+          element={
+            <div className={[styles.app, isOpen && styles.openAlert].join(' ')}>
+              <SideBar />
+              <ContentBlock />
+            </div>
+          }
+        />
+        <Route
+          path='/categories/:categoryName'
+          element={
+            <div className={[styles.app, isOpen && styles.openAlert].join(' ')}>
+              <SideBar />
+              <ContentBlock />
+            </div>
+          }
+        />
+        <Route
+          path='/trash'
+          element={
+            <div className={[styles.app, isOpen && styles.openAlert].join(' ')}>
+              <SideBar />
+              <ContentBlock />
+            </div>
+          }
+        />
+        <Route path='*' element={<h1>Страница не найдена</h1>} />
+      </Routes>
+      <Footer />
       {isOpen && <Alert message={message} toggleAlert={toggleAlert} />}
     </>
   );
