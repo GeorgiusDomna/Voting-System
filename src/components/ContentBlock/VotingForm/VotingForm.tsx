b55df14f1e43a3c5b27c3a9a57ec6dc3510af3b0
Decrast <71003027+skorohodov97@users.noreply.github.com>
2023-12-01 05:39:50 +0300

    Add files via upload

---
+++ src/components/ContentBlock/VotingForm/VotingForm.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import './VotingForm.css'; // Импортируем стили
+
+interface VotingFormProps {
+  documentId: string;
+}
+
+const VotingForm:React.FC<VotingFormProps> = ({ documentId }) => {
+  const handleSubmit = (values: { vote: string }) => {
+    // Здесь можно добавить логику для отправки данных на сервер
+    console.log(`Документ ID: ${documentId}, Выбор: ${values.vote}`);
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-header">Отправить докумен {documentId} на подписание?</h2>
+      <Formik initialValues={{ vote: '' }} onSubmit={handleSubmit}>
+        <Form>
+          <div className="form-label">
+            <label>
+              <Field type="radio" name="vote" value="yes" className="form-radio" />
+              Да
+            </label>
+          </div>
+          <div className="form-label">
+            <label>
+              <Field type="radio" name="vote" value="no" className="form-radio" />
+              Нет
+            </label>
+          </div>
+          <div>
+            <button type="submit" className="form-button">
+              Проголосовать
+            </button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default VotingForm;
