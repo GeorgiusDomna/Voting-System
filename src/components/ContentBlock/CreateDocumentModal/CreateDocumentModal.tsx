af5b11adf28770d0e328008980db4b2389de24bf
EvgeniiyaR <tulpansik@mail.ru>
2023-12-02 22:00:49 +0300

    feat: add CreateDocumentModal component

--- src/components/ContentBlock/CreateDocumentModal/CreateDocumentModal.tsx
+++ src/components/ContentBlock/CreateDocumentModal/CreateDocumentModal.tsx
@@ -1,12 +1,11 @@
 import Modal from 'react-modal';
 import { observer } from 'mobx-react-lite';
 import * as Yup from 'yup';
-import { Form, Formik } from 'formik';
+import { Form, Formik, FormikHelpers } from 'formik';
 import InputText from '@/components/Auth/Inputs/InputText';
 import styles from './createDocumentModal.module.css';
-import closeIcon from '@/assets/cancel.svg';
 import { useState } from 'react';
-import InputUpload from '@/components/Shared/InputUpload/InputUpload';
+import InputUpload from '@/components/ContentBlock/CreateDocumentModal/InputUpload/InputUpload';
 import authStore from '@/stores/AuthStore';
 import { createDoc, createFile } from '@/api/docuService';
 import alertStore from '@/stores/AlertStore';
@@ -14,6 +13,7 @@ import alertStore from '@/stores/AlertStore';
 interface ICreateDocumentModalProps {
   toggle: () => void;
   isOpen: boolean;
+  toggleConfirm: (id: number | null) => void;
 }
 
 interface values {
@@ -24,96 +24,129 @@ interface values {
 interface valuesImages {
   image: string;
   text: string;
+  id: number;
+}
+
+interface valuesFiles {
+  file: File;
+  id: number;
 }
 
 if (process.env.NODE_ENV !== 'test') Modal.setAppElement('#root');
 
-const CreateDocumentModal: React.FC<ICreateDocumentModalProps> = observer(({ isOpen, toggle }) => {
-  const [images, setImages] = useState<valuesImages[]>([]);
-  const [files, setFiles] = useState<File[]>([]);
-  const AddUserSchema = Yup.object().shape({
-    docname: Yup.string().min(2, 'Минимум 2 символа').required('Поле обязательно для заполнения'),
-    file: Yup.string().required('Загрузка файлов обязательна'),
-  });
+const CreateDocumentModal: React.FC<ICreateDocumentModalProps> = observer(
+  ({ isOpen, toggle, toggleConfirm }) => {
+    const [images, setImages] = useState<valuesImages[]>([]);
+    const [files, setFiles] = useState<valuesFiles[]>([]);
+    const [count, setCount] = useState<number>(0);
 
-  function handleSubmit(values: values) {
-    if (authStore.token) {
-      createDoc(authStore.token, values.docname)
-        .then((res) => {
-          Promise.all(
-            files.map((item) => {
-              if (authStore.token) {
-                return createFile(authStore.token, res.id, item);
-              }
-              return;
-            })
-          )
-            .then((result) => {
-              console.log(result);
-            })
-            .catch((error) => alertStore.toggleAlert(error));
-          console.log(res);
-        })
-        .catch((error) => alertStore.toggleAlert(error));
+    const CreateDocumentSchema = Yup.object().shape({
+      docname: Yup.string().min(2, 'Минимум 2 символа').required('Поле обязательно для заполнения'),
+      file: Yup.string().required('Загрузите файлы'),
+    });
+
+    function handleSubmit(values: values, { resetForm }: FormikHelpers<values>) {
+      if (authStore.token) {
+        createDoc(authStore.token, values.docname)
+          .then((res) => {
+            Promise.all(
+              files.map((item) => {
+                if (authStore.token) {
+                  return createFile(authStore.token, res.id, item.file);
+                }
+                return;
+              })
+            )
+              .then(() => {
+                resetForm();
+                toggle();
+                toggleConfirm(res.id);
+                setImages([]);
+                setFiles([]);
+              })
+              .catch((error) => alertStore.toggleAlert(error));
+          })
+          .catch((error) => alertStore.toggleAlert(error));
+      }
     }
-  }
 
-  return (
-    <Modal isOpen={isOpen} contentLabel='Модальное окно' className={styles.modal}>
-      <img src={closeIcon} className={styles.modal__close} onClick={toggle} />
-      <Formik
-        initialValues={{
-          docname: '',
-          file: '',
-        }}
-        validateOnMount
-        validateOnChange
-        validationSchema={AddUserSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ handleChange, values, isValid, errors, submitCount }) => (
-          <Form className={styles.form} name='createDoc'>
-            <InputText
-              type='text'
-              name='docname'
-              placeholder='Название документа'
-              value={values.docname}
-              error={errors.docname}
-              handleChange={handleChange}
-              submitCount={submitCount}
-            />
-            <ul
-              className={[styles.listImage, `${images.length > 3 && styles.listImage_scroll}`].join(
-                ' '
-              )}
-            >
-              {images.length > 0 &&
-                images.map((item, index) => (
-                  <li key={index} className={styles.image}>
-                    <img className={styles.preview} src={item.image} alt={item.text} />
-                  </li>
-                ))}
-            </ul>
-            <InputUpload
-              name='file'
-              placeholder='Название документа'
-              value={values.file}
-              error={errors.file}
-              handleChange={handleChange}
-              submitCount={submitCount}
-              images={images}
-              setImages={setImages}
-              filesImages={files}
-              setFiles={setFiles}
-            />
-            <button type='submit' className={styles.button} disabled={submitCount >= 1 && !isValid}>
-              Добавить документ
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </Modal>
-  );
-});
+    function deleteImage(id: number) {
+      setImages(images.filter((item) => item.id !== id));
+      setFiles(files.filter((item) => item.id !== id));
+    }
+
+    return (
+      <Modal isOpen={isOpen} contentLabel='Модальное окно' className={styles.modal}>
+        <button className={styles.modal__close} onClick={toggle} />
+        <Formik
+          initialValues={{
+            docname: '',
+            file: '',
+          }}
+          validateOnMount
+          validateOnChange
+          validationSchema={CreateDocumentSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ handleChange, values, isValid, errors, submitCount }) => (
+            <Form className={styles.form} name='createDoc'>
+              <InputText
+                type='text'
+                name='docname'
+                placeholder='Название документа'
+                value={values.docname}
+                error={errors.docname}
+                handleChange={handleChange}
+                submitCount={submitCount}
+              />
+              <ul
+                className={[
+                  styles.listImage,
+                  `${images.length > 6 && styles.listImage_scroll}`,
+                ].join(' ')}
+              >
+                {images.length > 0 &&
+                  images.map((item) => (
+                    <li key={item.id} className={styles.itemImage}>
+                      <button
+                        type='button'
+                        className={styles.deleteImage}
+                        onClick={() => deleteImage(item.id)}
+                      />
+                      <img className={styles.preview} src={item.image} alt={item.text} />
+                      <p className={styles.previewText} title={item.text}>
+                        {item.text}
+                      </p>
+                    </li>
+                  ))}
+              </ul>
+              <InputUpload
+                name='file'
+                placeholder='Название документа'
+                value={values.file}
+                error={errors.file}
+                handleChange={handleChange}
+                submitCount={submitCount}
+                images={images}
+                setImages={setImages}
+                filesImages={files}
+                setFiles={setFiles}
+                count={count}
+                setCount={setCount}
+              />
+              <button
+                type='submit'
+                className={styles.button}
+                disabled={submitCount >= 1 && !isValid}
+              >
+                Добавить документ
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </Modal>
+    );
+  }
+);
 
 export default CreateDocumentModal;
