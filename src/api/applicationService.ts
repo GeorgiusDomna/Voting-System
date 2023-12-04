9667f7d0c32d58e471a7d06bd32a07872072c589
Decrast <71003027+skorohodov97@users.noreply.github.com>
2023-12-05 01:43:00 +0300

    Add files via upload

--- src/api/applicationService.ts
+++ src/api/applicationService.ts
@@ -64,3 +64,35 @@ export async function connectDocToApplication(token: string, appId: number, docI
     return Promise.reject('Что-то пошло не так');
   }
 }
+
+/**
+ * Отправляет выбранный вариант ответа.
+ *
+ * @throws {NetworkError} Если ответ сервера не успешен, вызывается `alertStore.toggleAlert()` с сообщением об ошибке.
+ *
+ */
+export async function  handelsubmit(
+  token: string, appId: number, docId: number,
+  value: { status: string; comment: string }
+) {
+  try {
+    const url = `${baseUrl}/application/${appId}/applicationItem/${docId}/vote`;
+    if (!isOnline()) throw new NetworkError();
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(value),
+    });
+    if (!response.ok) {
+      const error: IFailedServerResponse = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  } catch (error) {
+    return Promise.reject('Что-то пошло не так');
+  }
+}
